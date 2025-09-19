163c7f7c9a8d358ead01d7f3e5ddc2d172973f52
Chung <chung@gmail.com>
2025-09-19 16:55:00 -0500

    Se creo el repository

---
+++ src/main/java/com/example/sistema_venta_chocotejas/SistemaVentaChocotejasApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistema_venta_chocotejas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVentaChocotejasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaVentaChocotejasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sistema_venta_chocotejas/SistemaVentaChocotejasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistema_venta_chocotejas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVentaChocotejasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
