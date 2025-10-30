c20c6f7c3e8c302da80e445ff942236ec912fa0f
Chungm7 <chungcoelhove070@gmail.com>
2025-10-30 10:54:47 -0500

    feat: Para todos los html de cliente se modifcaron los contacto y los iconos

--- src/main/java/com/example/sistema_venta_chocotejas/controller/cliente/GaleriaController.java
+++ src/main/java/com/example/sistema_venta_chocotejas/controller/cliente/GaleriaController.java
@@ -1,8 +1,12 @@
 package com.example.sistema_venta_chocotejas.controller.cliente;
 
 import com.example.sistema_venta_chocotejas.model.Categoria;
+import com.example.sistema_venta_chocotejas.model.Contacto;
 import com.example.sistema_venta_chocotejas.model.Producto;
+import com.example.sistema_venta_chocotejas.model.RedSocial;
 import com.example.sistema_venta_chocotejas.service.CategoriaService;
+import com.example.sistema_venta_chocotejas.service.Impl.ContactoServiceImpl;
+import com.example.sistema_venta_chocotejas.service.Impl.RedSocialServiceImpl;
 import com.example.sistema_venta_chocotejas.service.ProductoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,10 +21,17 @@ public class GaleriaController {
 
     private final ProductoService productoService;
     private final CategoriaService categoriaService;
+    private final ContactoServiceImpl contactoService;
+    private final RedSocialServiceImpl redSocialService;
 
-    public GaleriaController(ProductoService productoService, CategoriaService categoriaService) {
+    public GaleriaController(ProductoService productoService,
+                             CategoriaService categoriaService,
+                             ContactoServiceImpl contactoService,
+                             RedSocialServiceImpl redSocialService) {
         this.productoService = productoService;
         this.categoriaService = categoriaService;
+        this.contactoService = contactoService;
+        this.redSocialService = redSocialService;
     }
 
     @GetMapping("/productos")
@@ -28,9 +39,13 @@ public class GaleriaController {
         // Obtener productos activos (estado = 1) y categorías activas
         List<Producto> productosActivos = productoService.listarProductosActivos1();
         List<Categoria> categoriasActivas = categoriaService.listarCategorias1();
+        Contacto contacto = contactoService.obtenerContacto().orElse(null);
+        List<RedSocial> redSocials= redSocialService.listarRedesSocialesActivas();
 
         model.addAttribute("productos", productosActivos);
         model.addAttribute("categorias", categoriasActivas);
+        model.addAttribute("contacto", contacto);
+        model.addAttribute("redSocials", redSocials);
         return "client/galeria";
     }
 }
