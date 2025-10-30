1f472909757af2e693cda641e0f2380b241b2918
Chungm7 <chungcoelhove070@gmail.com>
2025-10-30 00:14:14 -0500

    Gestion de Inicio Realizado

--- src/main/java/com/example/sistema_venta_chocotejas/controller/cliente/TiendaController.java
+++ src/main/java/com/example/sistema_venta_chocotejas/controller/cliente/TiendaController.java
@@ -1,6 +1,8 @@
 package com.example.sistema_venta_chocotejas.controller.cliente;
 
+import com.example.sistema_venta_chocotejas.model.Inicio;
 import com.example.sistema_venta_chocotejas.model.Producto;
+import com.example.sistema_venta_chocotejas.service.Impl.InicioServiceImpl;
 import com.example.sistema_venta_chocotejas.service.ProductoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,18 +16,21 @@ import java.util.List;
 public class TiendaController {
 
     private final ProductoService productoService;
+    private final InicioServiceImpl inicioService;
 
-    public TiendaController(ProductoService productoService) {
+    public TiendaController(ProductoService productoService, InicioServiceImpl inicioService) {
         this.productoService = productoService;
+        this.inicioService = inicioService;
     }
 
     @GetMapping("/inicio")
     public String home(Model model) {
         List<Producto> productosDestacados = productoService.listarProductosDestacadosActivos();
-
+        Inicio inicio =  inicioService.obtenerInicio().orElse(null);
         // Verificar si hay más de 4 productos para mostrar controles de navegación
         boolean mostrarControles = productosDestacados.size() > 4;
 
+        model.addAttribute("inicio", inicio);
         model.addAttribute("productosDestacados", productosDestacados);
         model.addAttribute("mostrarControles", mostrarControles);
 
